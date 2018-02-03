a03c94190a38b23354479c4a91dc0c1519ca4f9d
Corey Morgan <wapitiasoft@gmail.com>
2018-02-03 13:21:01 -0700

    Partition polishing

--- modules/sievos-core/src/main/scala/org/sievos/kern/Partition.scala
+++ modules/sievos-core/src/main/scala/org/sievos/kern/Partition.scala
@@ -2,13 +2,29 @@ package org.sievos.kern
 
 /**
  * Part is a simple binary tree representation called here a Partition.
+ * Each Part is a node in the binary tree. A node may either be a
+ * LeafNode or a SplitNode (non-terminal node).
+ * 
+ * <p>The creation of the tree is strictly enforced by the Part constructor
+ * apply methods and by the extract/prepend/combine operations so that:
+ * 
+ * <ul>
+ * <li>Each LeafNode holds one value.
+ * <li>Each SplitNode holds two sub Parts, left and right. 
+ *     Both left and right are either a LeafNode or recursively another
+ *     SplitNode. They cannot be null or missing.
+ * <li>All nodes are immutable. To change a value, a new tree is constructed
+ *     as the old tree is traversed via extract/prepend/combine operations.
+ * </ul>
+ * 
  */
 trait Part[+T] {
   
-  
-  // partition context, a list of which leaf we're on during a visit,
-  // with the current lowest leaf's left or right branch would be a 0
-  // or 1 in List[0]
+  /**
+   * partition context. Each node has an implied position, a list indicating
+   * where it resides in its tree. The list is ordered from lowest to top-most 
+   * indices. The root node has a corresponding empty context list
+   */
   type PCTX = List[Int]
  
   /**
@@ -65,10 +81,9 @@ object Part {
   
   def apply[B](): Part[B] = EmptyNode
   
-  def apply[B](v: B): Part[B] = LeafNode[B](v)
+  def apply[B](v: B): Part[B] = LeafNode(v)
   
-  def apply[B](left: Part[B], v: B): Part[B] =
-    left | Part(v)
+  def apply[B](left: Part[B], v: B): Part[B] = left | Part(v)
   
   def apply[B](left: Part[B], right: Part[B]): Part[B] = {
     
@@ -137,7 +152,6 @@ object Part {
         
     override def append[B](item: B, tcomb: (B,B) => B): Part[B] = 
         Part(item)
-        
   }
   
   sealed private case class LeafNode[T](v: T) extends PartImpl[T] {
@@ -164,10 +178,6 @@ object Part {
   sealed private case class SplitNode[T](left: PartImpl[T], right: PartImpl[T]) 
     extends PartImpl[T] 
   {
-    // It is illegal for BiPart's left or right to be EmptyPart 
-    if (left == EmptyNode) throw new RuntimeException
-    if (right == EmptyNode) throw new RuntimeException
-   
     override def isSplit = true
     
     override def visit(ctx:PCTX, tconsumer: (T,PCTX) => Unit) {
