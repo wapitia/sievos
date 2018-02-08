26e099b27a222ec25949ca7b8b682228135e809b
wapitia <wapitiasoft@gmail.com>
2018-02-08 10:21:54 -0700

    comments and some clean-up

--- modules/sievos-core/src/main/java/com/wapitia/common/collections/LimitIterator.java
+++ modules/sievos-core/src/main/java/com/wapitia/common/collections/LimitIterator.java
@@ -29,41 +29,76 @@
  * ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF
  * WAPITIA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
  */
-
 package com.wapitia.common.collections;
 
 import java.util.Iterator;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 /**
  * Simple wrapping around another iterator imposing
  * some limit to how many elements are returned.
+ * <p>
+ * The client provides an {@see Iterator} that will be wrapped, and
+ * a {@see Predicate} whose job it is to cut off the iteration at a certain
+ * index point. The predicate is provided the 0-based count of the
+ * wrapped iterator's item that have been delivered so far,
+ * and the predicate's job is to return false once a threshold is met.
+ *
+ * <p>
+ * Example:
+ * <p>
+ * <pre>
+ *
+ *  Iterator<String> inputIter ...
+ *  Predicate<Long> first10 = (Long ix) -> ix < 10L;
+ *  Iterator<String> limIter = LimitIterator.<String>apply(inputIter, first10);
+ *
+ * </pre>
  *
  * @param <T> iterator element item
  */
 public class LimitIterator<T> implements Iterator<T> {
 
-    final Iterator<T> src;
+    final Iterator<T> sourceIter;
     final Predicate<Long> limitPredicate;
+
+    // mutable current index, starts at zero and increments on every next()
     long current;
 
-    public LimitIterator(final Iterator<T> src,
+    /**
+     * Constructor takes a source iterator to wrap and a predicate
+     * that checks against the current 0-based iterator index
+     * to tell whether to advance.
+     *
+     * @param sourceIter
+     * @param limitPredicate
+     */
+    public LimitIterator(final Iterator<T> sourceIter,
         final Predicate<Long> limitPredicate)
     {
-        this.src = src;
+        this.sourceIter = sourceIter;
         this.limitPredicate = limitPredicate;
         this.current = 0L;
     }
 
     @Override
     public boolean hasNext() {
-        return limitPredicate.test(current) && src.hasNext();
+        return limitPredicate.test(current) && sourceIter.hasNext();
     }
 
     @Override
     public T next() {
-        this.current = current + 1L;
-        return src.next();
+        this.current += 1L;
+        return sourceIter.next();
+    }
+
+    public static <TS> LimitIterator<TS> apply(final Iterator<TS> src,
+            final Predicate<Long> limitPredicate)
+    {
+        Objects.requireNonNull(src);
+        Objects.requireNonNull(limitPredicate);
+        return new LimitIterator<TS>(src, limitPredicate);
     }
 
 }
